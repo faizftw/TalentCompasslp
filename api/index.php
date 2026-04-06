bc890ade738b00b68d7cdf3ee3eaadafe478593c
faizftw <faizfatwa030206@gmail.com>
2026-04-06 13:33:56 +0800

    chore: configure application for Vercel deployment
    - Added `api/index.php` for serverless function handling
    - Created `vercel.json` configured with Vercel PHP and Static Build runtimes
    - Adjusted `bootstrap/app.php` to dynamically route storage caches to `/tmp` in Vercel environment

---
+++ api/index.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Forward Vercel requests to the normal Laravel application.
+ * This is required for deploying Laravel on Vercel using vercel-php.
+ */
+require __DIR__ . '/../public/index.php';
